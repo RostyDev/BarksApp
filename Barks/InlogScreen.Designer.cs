b1a0664894a5630d90a7d9a076b8682507ce0f97
RostyDev <minecraftwolfthijs@gmail.com>
2023-01-17 13:53:56 +0100

    Inlog scherm is niet responsive omdat we dat veranderd hebben

--- Barks/InlogScreen.Designer.cs
+++ Barks/InlogScreen.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.tb_Email = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.tb_UserWW = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_Inloggen = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,15 +49,6 @@
             this.panel1.Size = new System.Drawing.Size(438, 701);
             this.panel1.TabIndex = 0;
             // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel3.Location = new System.Drawing.Point(958, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(381, 701);
-            this.panel3.TabIndex = 4;
-            // 
             // panel2
             // 
             this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -67,6 +58,15 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(368, 701);
             this.panel2.TabIndex = 5;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(958, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(381, 701);
+            this.panel3.TabIndex = 4;
             // 
             // tb_Email
             // 
@@ -111,24 +111,27 @@
             this.label2.TabIndex = 8;
             this.label2.Text = "Password:";
             // 
-            // button1
+            // btn_Inloggen
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(616, 349);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(172, 100);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Inloggen";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btn_Inloggen.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_Inloggen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btn_Inloggen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Inloggen.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Inloggen.Location = new System.Drawing.Point(616, 349);
+            this.btn_Inloggen.Name = "btn_Inloggen";
+            this.btn_Inloggen.Size = new System.Drawing.Size(172, 100);
+            this.btn_Inloggen.TabIndex = 9;
+            this.btn_Inloggen.Text = "Inloggen";
+            this.btn_Inloggen.UseVisualStyleBackColor = false;
+            this.btn_Inloggen.Click += new System.EventHandler(this.btn_Inloggen_Click);
             // 
             // InlogScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Inloggen);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.tb_UserWW);
             this.Controls.Add(this.label1);
@@ -153,6 +156,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox tb_UserWW;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_Inloggen;
     }
 }
