9aecbbc217be3b77c2c8be5dda9fcb6ef8e67d0b
RostyDev <minecraftwolfthijs@gmail.com>
2023-01-17 14:17:04 +0100

    Basic veranderingen

--- BarksAPI/BasicDBfunctions.cs
+++ BarksAPI/BasicDBfunctions.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,8 @@ namespace BarksAPI
     {
         private void MakeConnection()
         {
-
+            MySqlConnection conn;
+            string Connectionstring = ConfigurationManager.AppSettings["LocalConn"];
         }
     }
 }
