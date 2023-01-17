6921a980e992fcb718c1e83aa1a6a7eeb31ff9d6
Christiaan Rozema <400041708@st.roc.a12.nl>
2023-01-17 14:35:04 +0100

    hoofdschrem

--- Barks/Form1.Designer.cs
+++ Barks/Form1.Designer.cs
@@ -30,10 +30,10 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.btn_FullScreen = new System.Windows.Forms.Button();
             this.btn_CloseForm = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.inlogScreen1 = new Barks.InlogScreen();
-            this.btn_FullScreen = new System.Windows.Forms.Button();
+            this.hooftmenu1 = new Barks.Hooftmenu();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -45,8 +45,9 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1231, 84);
+            this.panel1.Size = new System.Drawing.Size(1753, 68);
             this.panel1.TabIndex = 0;
             // 
             // panel2
@@ -54,10 +55,26 @@
             this.panel2.Controls.Add(this.btn_FullScreen);
             this.panel2.Controls.Add(this.btn_CloseForm);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(1099, 0);
+            this.panel2.Location = new System.Drawing.Point(1654, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(132, 84);
+            this.panel2.Size = new System.Drawing.Size(99, 68);
             this.panel2.TabIndex = 2;
+            // 
+            // btn_FullScreen
+            // 
+            this.btn_FullScreen.BackColor = System.Drawing.Color.Black;
+            this.btn_FullScreen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_FullScreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_FullScreen.ForeColor = System.Drawing.Color.Yellow;
+            this.btn_FullScreen.Location = new System.Drawing.Point(12, 18);
+            this.btn_FullScreen.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_FullScreen.Name = "btn_FullScreen";
+            this.btn_FullScreen.Size = new System.Drawing.Size(37, 37);
+            this.btn_FullScreen.TabIndex = 1;
+            this.btn_FullScreen.Text = "F";
+            this.btn_FullScreen.UseVisualStyleBackColor = false;
+            this.btn_FullScreen.Click += new System.EventHandler(this.btn_FullScreen_Click);
             // 
             // btn_CloseForm
             // 
@@ -65,9 +82,10 @@
             this.btn_CloseForm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_CloseForm.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_CloseForm.ForeColor = System.Drawing.Color.Yellow;
-            this.btn_CloseForm.Location = new System.Drawing.Point(71, 22);
+            this.btn_CloseForm.Location = new System.Drawing.Point(53, 18);
+            this.btn_CloseForm.Margin = new System.Windows.Forms.Padding(2);
             this.btn_CloseForm.Name = "btn_CloseForm";
-            this.btn_CloseForm.Size = new System.Drawing.Size(49, 45);
+            this.btn_CloseForm.Size = new System.Drawing.Size(37, 37);
             this.btn_CloseForm.TabIndex = 0;
             this.btn_CloseForm.Text = "X";
             this.btn_CloseForm.UseVisualStyleBackColor = false;
@@ -78,43 +96,30 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Yellow;
-            this.label1.Location = new System.Drawing.Point(24, 28);
+            this.label1.Location = new System.Drawing.Point(18, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 39);
+            this.label1.Size = new System.Drawing.Size(84, 31);
             this.label1.TabIndex = 1;
             this.label1.Text = "Barks";
             // 
-            // inlogScreen1
+            // hooftmenu1
             // 
-            this.inlogScreen1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.inlogScreen1.Location = new System.Drawing.Point(0, 84);
-            this.inlogScreen1.Name = "inlogScreen1";
-            this.inlogScreen1.Size = new System.Drawing.Size(1231, 556);
-            this.inlogScreen1.TabIndex = 1;
-            // 
-            // btn_FullScreen
-            // 
-            this.btn_FullScreen.BackColor = System.Drawing.Color.Black;
-            this.btn_FullScreen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_FullScreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_FullScreen.ForeColor = System.Drawing.Color.Yellow;
-            this.btn_FullScreen.Location = new System.Drawing.Point(16, 22);
-            this.btn_FullScreen.Name = "btn_FullScreen";
-            this.btn_FullScreen.Size = new System.Drawing.Size(49, 45);
-            this.btn_FullScreen.TabIndex = 1;
-            this.btn_FullScreen.Text = "F";
-            this.btn_FullScreen.UseVisualStyleBackColor = false;
-            this.btn_FullScreen.Click += new System.EventHandler(this.btn_FullScreen_Click);
+            this.hooftmenu1.Location = new System.Drawing.Point(0, 57);
+            this.hooftmenu1.Name = "hooftmenu1";
+            this.hooftmenu1.Size = new System.Drawing.Size(1856, 882);
+            this.hooftmenu1.TabIndex = 1;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.ClientSize = new System.Drawing.Size(1231, 640);
-            this.Controls.Add(this.inlogScreen1);
+            this.ClientSize = new System.Drawing.Size(1753, 872);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.hooftmenu1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -133,6 +138,7 @@
         private System.Windows.Forms.Panel panel2;
         private InlogScreen inlogScreen1;
         private System.Windows.Forms.Button btn_FullScreen;
+        private Hooftmenu hooftmenu1;
     }
 }
 
---
+++ Barks/Hooftmenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Barks
+{
+    public partial class Hooftmenu : UserControl
+    {
+        public Hooftmenu()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
