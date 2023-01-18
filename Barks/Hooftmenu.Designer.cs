c3a45caf843fccc31d97c055d61afe38fd41f771
RostyDev <minecraftwolfthijs@gmail.com>
2023-01-18 12:05:44 +0100

    pp

--- Barks/Hooftmenu.Designer.cs
+++ Barks/Hooftmenu.Designer.cs
@@ -49,24 +49,27 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(478, 779);
+            this.textBox1.Location = new System.Drawing.Point(451, 826);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(769, 31);
+            this.textBox1.Size = new System.Drawing.Size(569, 37);
             this.textBox1.TabIndex = 0;
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(155, 282);
+            this.richTextBox1.Location = new System.Drawing.Point(54, 347);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(317, 225);
+            this.richTextBox1.Size = new System.Drawing.Size(364, 193);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(155, 549);
+            this.richTextBox2.Location = new System.Drawing.Point(54, 609);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(317, 225);
+            this.richTextBox2.Size = new System.Drawing.Size(364, 254);
             this.richTextBox2.TabIndex = 2;
             this.richTextBox2.Text = "";
             // 
@@ -75,9 +78,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Yellow;
-            this.label1.Location = new System.Drawing.Point(150, 254);
+            this.label1.Location = new System.Drawing.Point(67, 312);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(111, 25);
+            this.label1.Size = new System.Drawing.Size(137, 31);
             this.label1.TabIndex = 3;
             this.label1.Text = "About me";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -87,26 +91,29 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Yellow;
-            this.label2.Location = new System.Drawing.Point(150, 521);
+            this.label2.Location = new System.Drawing.Point(67, 574);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(227, 25);
+            this.label2.Size = new System.Drawing.Size(281, 31);
             this.label2.TabIndex = 4;
             this.label2.Text = "Connected accounts";
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Yellow;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 161);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 198);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1777, 60);
+            this.pictureBox1.Size = new System.Drawing.Size(2369, 74);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
             // richTextBox3
             // 
-            this.richTextBox3.Location = new System.Drawing.Point(1248, 282);
+            this.richTextBox3.Location = new System.Drawing.Point(1038, 354);
+            this.richTextBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.richTextBox3.Name = "richTextBox3";
-            this.richTextBox3.Size = new System.Drawing.Size(317, 492);
+            this.richTextBox3.Size = new System.Drawing.Size(290, 515);
             this.richTextBox3.TabIndex = 6;
             this.richTextBox3.Text = "";
             // 
@@ -115,9 +122,10 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Yellow;
-            this.label3.Location = new System.Drawing.Point(1243, 254);
+            this.label3.Location = new System.Drawing.Point(1047, 312);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(176, 25);
+            this.label3.Size = new System.Drawing.Size(216, 31);
             this.label3.TabIndex = 7;
             this.label3.Text = "Also interesting";
             // 
@@ -125,9 +133,10 @@
             // 
             this.pictureBox2.BackgroundImage = global::Barks.Properties.Resources.Schermafbeelding_2022_11_10_164150;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(68, 72);
+            this.pictureBox2.Location = new System.Drawing.Point(32, 100);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(193, 169);
+            this.pictureBox2.Size = new System.Drawing.Size(257, 208);
             this.pictureBox2.TabIndex = 8;
             this.pictureBox2.TabStop = false;
             // 
@@ -136,9 +145,10 @@
             this.button1.BackColor = System.Drawing.Color.Yellow;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(343, 171);
+            this.button1.Location = new System.Drawing.Point(365, 210);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 40);
+            this.button1.Size = new System.Drawing.Size(197, 49);
             this.button1.TabIndex = 9;
             this.button1.Text = "Barks";
             this.button1.UseVisualStyleBackColor = false;
@@ -148,9 +158,10 @@
             this.button2.BackColor = System.Drawing.Color.Yellow;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(672, 171);
+            this.button2.Location = new System.Drawing.Point(606, 210);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(148, 40);
+            this.button2.Size = new System.Drawing.Size(197, 49);
             this.button2.TabIndex = 10;
             this.button2.Text = "Followers";
             this.button2.UseVisualStyleBackColor = false;
@@ -160,9 +171,10 @@
             this.button3.BackColor = System.Drawing.Color.Yellow;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(1046, 171);
+            this.button3.Location = new System.Drawing.Point(860, 210);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(148, 40);
+            this.button3.Size = new System.Drawing.Size(197, 49);
             this.button3.TabIndex = 11;
             this.button3.Text = "Following";
             this.button3.UseVisualStyleBackColor = false;
@@ -172,9 +184,10 @@
             this.button4.BackColor = System.Drawing.Color.Yellow;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(1417, 171);
+            this.button4.Location = new System.Drawing.Point(1107, 210);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(148, 40);
+            this.button4.Size = new System.Drawing.Size(197, 49);
             this.button4.TabIndex = 12;
             this.button4.Text = "Reports";
             this.button4.UseVisualStyleBackColor = false;
@@ -184,15 +197,16 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Yellow;
-            this.label4.Location = new System.Drawing.Point(267, 119);
+            this.label4.Location = new System.Drawing.Point(356, 146);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(238, 39);
+            this.label4.Size = new System.Drawing.Size(298, 52);
             this.label4.TabIndex = 13;
             this.label4.Text = "FarmerFan69";
             // 
             // Hooftmenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
             this.Controls.Add(this.label4);
@@ -209,8 +223,9 @@
             this.Controls.Add(this.richTextBox2);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Hooftmenu";
-            this.Size = new System.Drawing.Size(1777, 859);
+            this.Size = new System.Drawing.Size(1353, 873);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
