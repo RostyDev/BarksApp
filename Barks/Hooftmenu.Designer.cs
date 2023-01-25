b1630c961210ee09138b14501137690ef253f929
RostyDev <minecraftwolfthijs@gmail.com>
2023-01-25 20:47:40 +0100

    Small UI changes

--- Barks/Hooftmenu.Designer.cs
+++ Barks/Hooftmenu.Designer.cs
@@ -44,6 +44,7 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btn_updateBio = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -52,22 +53,22 @@
             // 
             this.tb_text.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.tb_text.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_text.Location = new System.Drawing.Point(427, 792);
-            this.tb_text.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_text.Location = new System.Drawing.Point(320, 644);
             this.tb_text.Multiline = true;
             this.tb_text.Name = "tb_text";
-            this.tb_text.Size = new System.Drawing.Size(533, 71);
+            this.tb_text.Size = new System.Drawing.Size(401, 58);
             this.tb_text.TabIndex = 0;
             this.tb_text.Text = "inhoud:";
             // 
             // rh_bio
             // 
             this.rh_bio.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.rh_bio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.rh_bio.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rh_bio.Location = new System.Drawing.Point(53, 348);
-            this.rh_bio.Margin = new System.Windows.Forms.Padding(4);
+            this.rh_bio.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.rh_bio.Location = new System.Drawing.Point(40, 283);
             this.rh_bio.Name = "rh_bio";
-            this.rh_bio.Size = new System.Drawing.Size(364, 436);
+            this.rh_bio.Size = new System.Drawing.Size(274, 355);
             this.rh_bio.TabIndex = 1;
             this.rh_bio.Text = "";
             // 
@@ -76,20 +77,20 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Yellow;
-            this.label1.Location = new System.Drawing.Point(67, 313);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(50, 254);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(137, 31);
+            this.label1.Size = new System.Drawing.Size(111, 25);
             this.label1.TabIndex = 3;
             this.label1.Text = "About me";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // richTextBox3
             // 
-            this.richTextBox3.Location = new System.Drawing.Point(1037, 347);
-            this.richTextBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.richTextBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.richTextBox3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.richTextBox3.Location = new System.Drawing.Point(778, 282);
             this.richTextBox3.Name = "richTextBox3";
-            this.richTextBox3.Size = new System.Drawing.Size(289, 522);
+            this.richTextBox3.Size = new System.Drawing.Size(218, 425);
             this.richTextBox3.TabIndex = 6;
             this.richTextBox3.Text = "";
             // 
@@ -98,10 +99,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Yellow;
-            this.label3.Location = new System.Drawing.Point(1047, 313);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(785, 254);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(216, 31);
+            this.label3.Size = new System.Drawing.Size(176, 25);
             this.label3.TabIndex = 7;
             this.label3.Text = "Also interesting";
             // 
@@ -110,10 +110,9 @@
             this.button1.BackColor = System.Drawing.Color.Yellow;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(365, 210);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(274, 171);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(197, 49);
+            this.button1.Size = new System.Drawing.Size(148, 40);
             this.button1.TabIndex = 9;
             this.button1.Text = "Barks";
             this.button1.UseVisualStyleBackColor = false;
@@ -123,10 +122,9 @@
             this.button2.BackColor = System.Drawing.Color.Yellow;
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(605, 210);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(454, 171);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(197, 49);
+            this.button2.Size = new System.Drawing.Size(148, 40);
             this.button2.TabIndex = 10;
             this.button2.Text = "Followers";
             this.button2.UseVisualStyleBackColor = false;
@@ -136,10 +134,9 @@
             this.button3.BackColor = System.Drawing.Color.Yellow;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(860, 210);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Location = new System.Drawing.Point(645, 171);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(197, 49);
+            this.button3.Size = new System.Drawing.Size(148, 40);
             this.button3.TabIndex = 11;
             this.button3.Text = "Following";
             this.button3.UseVisualStyleBackColor = false;
@@ -149,10 +146,9 @@
             this.button4.BackColor = System.Drawing.Color.Yellow;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(1107, 210);
-            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Location = new System.Drawing.Point(830, 171);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(197, 49);
+            this.button4.Size = new System.Drawing.Size(148, 40);
             this.button4.TabIndex = 12;
             this.button4.Text = "Reports";
             this.button4.UseVisualStyleBackColor = false;
@@ -162,34 +158,33 @@
             this.lbl_accountname.AutoSize = true;
             this.lbl_accountname.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_accountname.ForeColor = System.Drawing.Color.Yellow;
-            this.lbl_accountname.Location = new System.Drawing.Point(356, 146);
-            this.lbl_accountname.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_accountname.Location = new System.Drawing.Point(267, 119);
             this.lbl_accountname.Name = "lbl_accountname";
-            this.lbl_accountname.Size = new System.Drawing.Size(217, 52);
+            this.lbl_accountname.Size = new System.Drawing.Size(175, 39);
             this.lbl_accountname.TabIndex = 13;
             this.lbl_accountname.Text = "nickname";
             // 
             // btn_verzend
             // 
             this.btn_verzend.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btn_verzend.BackColor = System.Drawing.Color.Yellow;
+            this.btn_verzend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_verzend.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_verzend.Location = new System.Drawing.Point(971, 826);
-            this.btn_verzend.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_verzend.Location = new System.Drawing.Point(728, 671);
             this.btn_verzend.Name = "btn_verzend";
-            this.btn_verzend.Size = new System.Drawing.Size(59, 38);
+            this.btn_verzend.Size = new System.Drawing.Size(44, 31);
             this.btn_verzend.TabIndex = 14;
             this.btn_verzend.Text = ">";
-            this.btn_verzend.UseVisualStyleBackColor = true;
+            this.btn_verzend.UseVisualStyleBackColor = false;
             this.btn_verzend.Click += new System.EventHandler(this.btn_verzend_Click);
             // 
             // tb_titel
             // 
             this.tb_titel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.tb_titel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tb_titel.Location = new System.Drawing.Point(425, 747);
-            this.tb_titel.Margin = new System.Windows.Forms.Padding(4);
+            this.tb_titel.Location = new System.Drawing.Point(319, 607);
             this.tb_titel.Name = "tb_titel";
-            this.tb_titel.Size = new System.Drawing.Size(533, 37);
+            this.tb_titel.Size = new System.Drawing.Size(401, 31);
             this.tb_titel.TabIndex = 15;
             this.tb_titel.Text = "Titel:";
             // 
@@ -197,49 +192,63 @@
             // 
             this.flp_EigenBarks.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.flp_EigenBarks.AutoScroll = true;
-            this.flp_EigenBarks.Location = new System.Drawing.Point(427, 347);
-            this.flp_EigenBarks.Margin = new System.Windows.Forms.Padding(4);
+            this.flp_EigenBarks.Location = new System.Drawing.Point(320, 282);
             this.flp_EigenBarks.Name = "flp_EigenBarks";
-            this.flp_EigenBarks.Size = new System.Drawing.Size(603, 392);
+            this.flp_EigenBarks.Size = new System.Drawing.Size(452, 318);
             this.flp_EigenBarks.TabIndex = 16;
             // 
             // pictureBox2
             // 
             this.pictureBox2.BackgroundImage = global::Barks.Properties.Resources.geen_profielfoto_man_silhouet_pasfoto_anoniem_600x600_1;
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(53, 101);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Location = new System.Drawing.Point(40, 82);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(257, 208);
+            this.pictureBox2.Size = new System.Drawing.Size(193, 169);
             this.pictureBox2.TabIndex = 8;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Yellow;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 198);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 161);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(2369, 74);
+            this.pictureBox1.Size = new System.Drawing.Size(1777, 60);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
             // btn_updateBio
             // 
             this.btn_updateBio.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.btn_updateBio.Location = new System.Drawing.Point(53, 791);
+            this.btn_updateBio.BackColor = System.Drawing.Color.Yellow;
+            this.btn_updateBio.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_updateBio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_updateBio.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_updateBio.Location = new System.Drawing.Point(40, 643);
+            this.btn_updateBio.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_updateBio.Name = "btn_updateBio";
-            this.btn_updateBio.Size = new System.Drawing.Size(364, 72);
+            this.btn_updateBio.Size = new System.Drawing.Size(273, 58);
             this.btn_updateBio.TabIndex = 17;
             this.btn_updateBio.Text = "Update Bio";
-            this.btn_updateBio.UseVisualStyleBackColor = true;
+            this.btn_updateBio.UseVisualStyleBackColor = false;
             this.btn_updateBio.Click += new System.EventHandler(this.btn_updateBio_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Yellow;
+            this.label2.Location = new System.Drawing.Point(477, 254);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 25);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Jouw barks";
             // 
             // Hooftmenu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.btn_updateBio);
             this.Controls.Add(this.flp_EigenBarks);
             this.Controls.Add(this.tb_titel);
@@ -256,9 +265,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.rh_bio);
             this.Controls.Add(this.tb_text);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Hooftmenu";
-            this.Size = new System.Drawing.Size(1353, 873);
+            this.Size = new System.Drawing.Size(1015, 709);
             this.Load += new System.EventHandler(this.Hooftmenu_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -285,5 +293,6 @@
         private System.Windows.Forms.TextBox tb_titel;
         public System.Windows.Forms.FlowLayoutPanel flp_EigenBarks;
         private System.Windows.Forms.Button btn_updateBio;
+        private System.Windows.Forms.Label label2;
     }
 }
